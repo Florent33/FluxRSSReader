d99f853eb4f7adc33f482c73018ff0732112ad91
Administrateur <administrateur@macbook-pro-de-administrateur.local>
2015-12-26 22:12:09 +0100

    Commit APPMasterViewController

---
+++ FluxRSSReader/APPMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  APPMasterViewController.h
+//  FluxRSSReader
+//
+//  Created by Florent FERRAND on 23/12/2015.
+//  Copyright © 2015 com.epsi.projeti5.fluxrssreader. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APPMasterViewController : UITableViewController
+
+@end
